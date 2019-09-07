975b152cfb4a268f815a8876ab43b825c620ce56
Wynterwolf <Jordan>
2019-09-07 17:49:48 -0500

    adding files

---
+++ conBodyFunctions.js
@@ -0,0 +1,8 @@
+/* const plantNeedsWater = (day) => {
+  return day === 'Wednesday' ? true : false;
+}; */
+
+
+//refactor with concise body
+
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
