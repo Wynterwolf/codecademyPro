6f3cbf13b25ad783b367470c4d6220e2ca314efc
Wynterwolf <shinyguns@gmail.com>
2019-10-15 15:25:56 -0500

    simplified and working

--- justCoolStuff.js
+++ justCoolStuff.js
@@ -1,25 +1,18 @@
-/* Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays */
+// Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays.
+// You can test your function when you’re ready by passing in the myStuff and coolStuff arrays or by making arrays of your own!
 
-//Your Code Here
+// Write your code here:
 
-const justCoolStuff = 
-
-const coolStuffList = []
-for (let cs = 0; cs < coolStuffList.length; cs++) {
-  for (let ms = 0; ms < myStuff.length; ms++) {
-    if (coolStuffList[cs] === myStuff[ms]){
-      coolStuffList.push(coolStuffList[cs]);
-    }
-  }
-}
-
-
-// Feel free to uncomment the code below to test your function
-
-const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
-
-const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
-
-console.log(justCoolStuff(myStuff, coolStuff))
-// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
-
+const justCoolStuff = (array1, array2) => {
+    return array1.filter(item => array2.includes(item))
+  };
+  // Feel free to uncomment the code below to test your function
+  
+  const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+  
+  const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+  
+  console.log(justCoolStuff(myStuff, coolStuff))
+  // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+  
+  
