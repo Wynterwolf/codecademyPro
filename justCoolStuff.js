e990924ee631136d6572396949a39d64a5975c86
Wynterwolf <shinyguns@gmail.com>
2019-10-15 13:22:50 -0500

    adding

---
+++ justCoolStuff.js
@@ -0,0 +1,16 @@
+/* Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays */
+
+//Your Code Here
+
+
+
+// Feel free to uncomment the code below to test your function
+/*
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+
+*/
