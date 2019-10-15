a7d03b46967c21e6d0a0a9fcc01175648563278d
Wynterwolf <shinyguns@gmail.com>
2019-10-15 12:38:40 -0500

    works but not passing test

--- sortYears.js
+++ sortYears.js
@@ -1,10 +1,6 @@
-/* Write a function sortYears() that takes in an array of years, and, using the built-in .sort() method, returns that array with the years sorted in descending order.
-
-You can test your function when you’re ready by passing in the years array or by making your own array of years! */
-
 // Write your code here:
 
-const sortYears = array => array.map(array => years.sort());
+const sortYears = array => array.sort((year1, year2) => year1 > year2);
 
 
 
@@ -16,4 +12,5 @@ const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
 console.log(sortYears(years))
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
+
 
