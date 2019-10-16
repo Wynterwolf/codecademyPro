97dcb3d479486a00c6c3fa971f605d9665d9f002
Lem Canady <lem.canady@gmail.com>
2019-10-16 13:01:41 -0700

    Solution!

--- isTheDinnerVegan.js
+++ isTheDinnerVegan.js
@@ -1,32 +1,38 @@
 // Write a function isTheDinnerVegan() that takes in an array of food objects in the format:
 
-{name: 'cabbage', source: 'plant' }
-
 // and returns a boolean value based on whether or not every item in the array has entirely plant-based origins.
 
-const meal = [{name: 'arugula', source: 'plant'}, {name: 'tomatoes', source: 'plant'}, {name: 'lemon', source:'plant'}, {name: 'olive oil', source: 'plant'}];
+const meal = [
+  { name: "arugula", source: "plant" },
+  { name: "tomatoes", source: "plant" },
+  { name: "lemon", source: "plant" },
+  { name: "olive oil", source: "plant" }
+];
 
-isTheDinnerVegan(meal); // Should return true
+// isTheDinnerVegan(meal); // Should return true
 
-// You can test your function when you’re ready by passing in the dinner array or by making your own! 
+// You can test your function when you’re ready by passing in the dinner array or by making your own!
 
 // Write your code here:
-const isTheDinnerVegan = (array) => {
-    if (dinner.source === 'plant') {
-      return true
-    }
-  return false;
-  }
-  
-  
-  
-  
-  
-  
-  
-  // Feel free to comment out the code below to test your function
-  
-  const dinner = [{name: 'hamburger', source: 'plant'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
-  
-  console.log(isTheDinnerVegan(dinner))
-  // Should print false
+// const isTheDinnerVegan = (array) => {
+//     if (dinner.source === 'plant') {
+//       return true
+//     }
+//   return false;
+//   }
+
+const isTheDinnerVegan = meal =>
+  meal.reduce((_, curr) => (curr.source === "plant" ? true : false), false);
+
+// Feel free to comment out the code below to test your function
+
+const dinner = [
+  { name: "hamburger", source: "plant" },
+  { name: "cheese", source: "dairy" },
+  { name: "ketchup", source: "plant" },
+  { name: "bun", source: "plant" },
+  { name: "dessert twinkies", source: "unknown" }
+];
+
+console.log(isTheDinnerVegan(meal));
+// Should print false
